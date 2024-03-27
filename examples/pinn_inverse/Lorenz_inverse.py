bf89e0f84acb0163f948c5f13f09734ead28f8b5
bonneted <bonneteymard.damien@gmail.com>
2024-03-27 10:48:50 +0100

    test jax inverse

--- examples/pinn_inverse/Lorenz_inverse.py
+++ examples/pinn_inverse/Lorenz_inverse.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def gen_traindata():
-    data = np.load("../dataset/Lorenz.npz")
+    data = np.load("./examples/dataset/Lorenz.npz")
     return data["t"], data["y"]
 
 
@@ -13,36 +13,36 @@ C2 = dde.Variable(1.0)
 C3 = dde.Variable(1.0)
 
 # Most backends
-def Lorenz_system(x, y):
-    """Lorenz system.
-    dy1/dx = 10 * (y2 - y1)
-    dy2/dx = y1 * (15 - y3) - y2
-    dy3/dx = y1 * y2 - 8/3 * y3
-    """
-    y1, y2, y3 = y[:, 0:1], y[:, 1:2], y[:, 2:]
-    dy1_x = dde.grad.jacobian(y, x, i=0)
-    dy2_x = dde.grad.jacobian(y, x, i=1)
-    dy3_x = dde.grad.jacobian(y, x, i=2)
-    return [
-        dy1_x - C1 * (y2 - y1),
-        dy2_x - y1 * (C2 - y3) + y2,
-        dy3_x - y1 * y2 + C3 * y3,
-    ]
-
-
-# Backend JAX
-# def Lorenz_system(x, y, unknowns=[C1, C2, C3]):
-#     C1, C2, C3 = unknowns
-#     y_val, y_fn = y
-#     y1, y2, y3 = y_val[:, 0:1], y_val[:, 1:2], y_val[:, 2:3]
-#     dy1_x, _ = dde.grad.jacobian(y, x, i=0)
-#     dy2_x, _ = dde.grad.jacobian(y, x, i=1)
-#     dy3_x, _ = dde.grad.jacobian(y, x, i=2)
+# def Lorenz_system(x, y):
+#     """Lorenz system.
+#     dy1/dx = 10 * (y2 - y1)
+#     dy2/dx = y1 * (15 - y3) - y2
+#     dy3/dx = y1 * y2 - 8/3 * y3
+#     """
+#     y1, y2, y3 = y[:, 0:1], y[:, 1:2], y[:, 2:]
+#     dy1_x = dde.grad.jacobian(y, x, i=0)
+#     dy2_x = dde.grad.jacobian(y, x, i=1)
+#     dy3_x = dde.grad.jacobian(y, x, i=2)
 #     return [
 #         dy1_x - C1 * (y2 - y1),
 #         dy2_x - y1 * (C2 - y3) + y2,
 #         dy3_x - y1 * y2 + C3 * y3,
 #     ]
+
+
+# Backend JAX
+def Lorenz_system(x, y, unknowns=[C1, C2, C3]):
+    C1, C2, C3 = unknowns
+    y_val, y_fn = y
+    y1, y2, y3 = y_val[:, 0:1], y_val[:, 1:2], y_val[:, 2:3]
+    dy1_x, _ = dde.grad.jacobian(y, x, i=0)
+    dy2_x, _ = dde.grad.jacobian(y, x, i=1)
+    dy3_x, _ = dde.grad.jacobian(y, x, i=2)
+    return [
+        dy1_x - C1 * (y2 - y1),
+        dy2_x - y1 * (C2 - y3) + y2,
+        dy3_x - y1 * y2 + C3 * y3,
+    ]
 
 
 def boundary(_, on_initial):
@@ -64,7 +64,7 @@ observe_y2 = dde.icbc.PointSetBC(observe_t, ob_y[:, 2:3], component=2)
 
 data = dde.data.PDE(
     geom,
-    Lorenz_system,
+    Lorenz_system,,
     [ic1, ic2, ic3, observe_y0, observe_y1, observe_y2],
     num_domain=400,
     num_boundary=2,
@@ -83,10 +83,12 @@ variable = dde.callbacks.VariableValue(
 model.compile(
     "adam", lr=0.001, external_trainable_variables=external_trainable_variables
 )
-losshistory, train_state = model.train(iterations=20000, callbacks=[variable])
+print(f"C1: {C1}, C2: {C2}, C3: {C3}")
+losshistory, train_state = model.train(iterations=1000, callbacks=[variable])
+print(f"C1: {C1}, C2: {C2}, C3: {C3}")
 
 # train lbfgs
-model.compile("L-BFGS", external_trainable_variables=external_trainable_variables)
-losshistory, train_state = model.train(callbacks=[variable])
+# model.compile("L-BFGS", external_trainable_variables=external_trainable_variables)
+# losshistory, train_state = model.train(callbacks=[variable])
 
-dde.saveplot(losshistory, train_state, issave=True, isplot=True)
+# dde.saveplot(losshistory, train_state, issave=True, isplot=True)
